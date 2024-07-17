cec29887e65fb7137de214a958466c1d596df066
Kseniya Semenenko <165210222+KseniyaSemenenko@users.noreply.github.com>
2024-07-17 21:21:46 +0300

    add task 1, 2

---
+++ js/task-1.js
@@ -0,0 +1,13 @@
+// const number = document.querySelectorAll(".item").length;
+// console.log("Number of categories: ", number)
+
+// const items = document.querySelectorAll('.item');
+
+// items.forEach(
+//     item => {
+//         const categoriesName = item.querySelector("h2").textContent; 
+//         console.log(`Category: ${categoriesName}`)
+//         const elements = item.querySelectorAll("li");
+//         console.log("Elements: ", elements.length)
+//     }
+// )
