04bdd4e12548f586b626c1b4d27002f63b5c23e1
Kseniya Semenenko <165210222+KseniyaSemenenko@users.noreply.github.com>
2024-07-20 09:42:11 +0300

    add task-6

--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,33 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+document.querySelector('[data-create]').addEventListener('click', () => {
+    const count = Number(document.querySelector('#controls input').value);
+    if (count >= 1 && count <= 100) {
+        destroyBoxes();
+        createBoxes(count);
+        document.querySelector('#controls input').value = '';
+    }
+});
+
+document.querySelector('[data-destroy]').addEventListener('click', destroyBoxes);
+
+function createBoxes(amount) {
+    const boxesContainer = document.querySelector('#boxes');
+    let size = 30;
+
+    for (let i = 0; i < amount; i++) {
+        const newElement = document.createElement('div');
+        newElement.classList.add('element');
+        newElement.style.width = `${size}px`;
+        newElement.style.height = `${size}px`;
+        newElement.style.backgroundColor = getRandomHexColor();
+        boxesContainer.appendChild(newElement);
+        size += 10; 
+    }
+}
+
+function destroyBoxes() {
+    document.querySelector('#boxes').innerHTML = ''; 
+}
+
