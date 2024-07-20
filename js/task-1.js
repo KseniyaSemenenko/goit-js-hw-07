d61ac308ed9fdce35cd299ae25983e31ba1e15a2
Kseniya Semenenko <165210222+KseniyaSemenenko@users.noreply.github.com>
2024-07-20 10:02:42 +0300

    update

--- js/task-1.js
+++ js/task-1.js
@@ -1,13 +1,13 @@
-// const number = document.querySelectorAll(".item").length;
-// console.log("Number of categories: ", number)
+const number = document.querySelectorAll(".item").length;
+console.log("Number of categories: ", number)
 
-// const items = document.querySelectorAll('.item');
+const items = document.querySelectorAll('.item');
 
-// items.forEach(
-//     item => {
-//         const categoriesName = item.querySelector("h2").textContent; 
-//         console.log(`Category: ${categoriesName}`)
-//         const elements = item.querySelectorAll("li");
-//         console.log("Elements: ", elements.length)
-//     }
-// )
+items.forEach(
+    item => {
+        const categoriesName = item.querySelector("h2").textContent; 
+        console.log(`Category: ${categoriesName}`)
+        const elements = item.querySelectorAll("li");
+        console.log("Elements: ", elements.length)
+    }
+)
